2b4d2bc510671839b491ac3d8fce47fb489d612e
JStation <6457979+JStation@users.noreply.github.com>
2022-08-10 15:05:18 -0700

    add Gameboard module

--- tictactoe.js
+++ tictactoe.js
@@ -1,1 +1,21 @@
 console.log("tictactoe.js loaded")
+
+/* 
+Note from project page:
+Minimize global code by using modules and factories
+If there is one of something use a module
+If there are multiples create them with factories
+*/
+
+// IIFE - Immediately Invoked Function Expression
+// Module pattern carves namespace for gameBoard operations/data
+const Gameboard = (() => {
+    const board = new Array(9).fill(null);
+
+    const getBoard = () => board;
+
+    // return exposed functions and data (public)
+    return {
+        getBoard,
+    };
+})();
